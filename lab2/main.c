5b0dc1e755b124d09d9cec5bdd87c9020424b68f
Rabiann <93538032+Rabiann@users.noreply.github.com>
2025-09-21 22:32:35 +0300

    add: makefile

--- lab2/main.c
+++ lab2/main.c
@@ -3,12 +3,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    float f;
     if (argc == 1)
         fprintf(stderr, "please provide numerical for calculation\n");
     else if (argc > 2)
         fprintf(stderr, "too many arguments\n");
+    else if (sscanf(argv[1], "%f", &f) != 1)
+        fprintf(stderr, "argument is not a valid float\n");
     else {
         float b = atof(argv[1]);
+        if (b < 2) {
+            fprintf(stderr, "value can't be less than 2");
+            return 1;
+        }
+
         float z1 = sqrt(2 * b + 2 * sqrt(b * b - 4));
         float z2 = 1 / sqrt(b + 2);
 
